b60019ad02c49e7fc3175466bca7adc975abcc6d
balusena <balusena12@gmail.com>
2024-03-21 17:23:23 +0000

    roboshop kubernetes files

--- run.sh
+++ run.sh
@@ -2,32 +2,32 @@
 
 set -x
 
-while true ; do
-  if [ -f /data/params ]; then
+while true; do
+  if [ -f "/data/params" ]; then
     echo "### Parameters"
-    cat /data/params
-    source /data/params
+    cat "/data/params"
+    source "/data/params"
     break
   else
-    echo $(date) - Waiting for Parameters
+    echo "$(date) - Waiting for Parameters"
     sleep 5
   fi
 done
 
-mkdir /app
-cd /app
-git clone https://github.com/balusena/${COMPONENT}
-cd ${COMPONENT}/schema
+mkdir "/app"
+cd "/app" || exit 1
+git clone "https://github.com/balusena/${COMPONENT}" || exit 1
+cd "${COMPONENT}/schema" || exit 1
 
 if [ "${SCHEMA_TYPE}" == "mongo" ]; then
-  curl -s -L https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem -o /app/rds-combined-ca-bundle.pem
-  mongo --ssl --host ${DOCDB_ENDPOINT}:27017 --sslCAFile /app/rds-combined-ca-bundle.pem --username ${DOCDB_USERNAME} --password ${DOCDB_PASSWORD} <${COMPONENT}.js
+  curl -s -L "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem" -o "/app/rds-combined-ca-bundle.pem" || exit 1
+  mongo --ssl --host "${DOCDB_ENDPOINT}:27017" --sslCAFile "/app/rds-combined-ca-bundle.pem" --username "${DOCDB_USERNAME}" --password "${DOCDB_PASSWORD}" <"${COMPONENT}.js" || exit 1
 elif [ "${SCHEMA_TYPE}" == "mysql" ]; then
-  echo show databases | mysql -h ${MYSQL_ENDPOINT} -u${MYSQL_USERNAME} -p${MYSQL_PASSWORD} | grep cities
+  echo "show databases" | mysql -h "${MYSQL_ENDPOINT}" -u"${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" | grep cities
   if [ $? -ne 0 ]; then
-    mysql -h ${MYSQL_ENDPOINT} -u${MYSQL_USERNAME} -p${MYSQL_PASSWORD} <${COMPONENT}.sql
+    mysql -h "${MYSQL_ENDPOINT}" -u"${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" <"${COMPONENT}.sql" || exit 1
   fi
 else
-  echo Invalid Schema Input
+  echo "Invalid Schema Input"
   exit 1
 fi
