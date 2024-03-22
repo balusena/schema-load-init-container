15aa5eb13bee7fe82315222e9b3ec90dc5021c60
balusena <balusena12@gmail.com>
2024-03-22 15:14:31 +0000

    roboshop kubernetes files

--- run.sh
+++ run.sh
@@ -17,15 +17,15 @@ done
 mkdir /app
 cd /app
 git clone "https://github.com/balusena/${COMPONENT}"
-cd "${COMPONENT}/schema"
+cd "${COMPONENT}"
 
 if [ "${SCHEMA_TYPE}" == "mongo" ]; then
   curl -s -L "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem" -o "/app/rds-combined-ca-bundle.pem"
-  mongo --ssl --host "${DOCDB_ENDPOINT}:27017" --sslCAFile "/app/rds-combined-ca-bundle.pem" --username "${DOCDB_USERNAME}" --password "${DOCDB_PASSWORD}" <"${COMPONENT}.js"
+  mongo --ssl --host "${DOCDB_ENDPOINT}:27017" --sslCAFile "/app/rds-combined-ca-bundle.pem" --username "${DOCDB_USERNAME}" --password "${DOCDB_PASSWORD}" <schema/"${COMPONENT}.js"
 elif [ "${SCHEMA_TYPE}" == "mysql" ]; then
   echo "show databases" | mysql -h "${MYSQL_ENDPOINT}" -u"${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" | grep cities
   if [ $? -ne 0 ]; then
-    mysql -h "${MYSQL_ENDPOINT}" -u"${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" <"${COMPONENT}.sql"
+    mysql -h "${MYSQL_ENDPOINT}" -u"${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" <schema/"${COMPONENT}.sql"
   fi
 else
   echo "Invalid Schema Input"
