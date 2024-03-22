2fda47ab88116cd2551ed1b5fbaac928a8e87a41
balusena <balusena12@gmail.com>
2024-03-22 15:45:10 +0000

    roboshop kubernetes files

--- run.sh
+++ run.sh
@@ -20,8 +20,8 @@ git clone "https://github.com/balusena/${COMPONENT}"
 cd "${COMPONENT}/schema"
 
 if [ "${SCHEMA_TYPE}" == "mongo" ]; then
-  curl -L "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem" -o "/app/global-bundle.pem"
-  mongo --ssl --host "${DOCDB_ENDPOINT}:27017" --sslCAFile "/app/global-bundle.pem" --username "${DOCDB_USERNAME}" --password "${DOCDB_PASSWORD}" <"${COMPONENT}.js"
+  curl -L "https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem" -o "/app/rds-combined-ca-bundle.pem"
+  mongo --ssl --host "${DOCDB_ENDPOINT}:27017" --sslCAFile "/app/rds-combined-ca-bundle.pem" --username "${DOCDB_USERNAME}" --password "${DOCDB_PASSWORD}" <"${COMPONENT}.js"
 elif [ "${SCHEMA_TYPE}" == "mysql" ]; then
   echo "show databases" | mysql -h "${MYSQL_ENDPOINT}" -u"${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" | grep cities
   if [ $? -ne 0 ]; then
