dbbc313df8aff73f513a0bc07af9507cf79ab202
balusena <balusena12@gmail.com>
2024-03-23 12:53:01 +0000

    roboshop kubernetes files

--- run.sh
+++ run.sh
@@ -10,9 +10,9 @@ source /data/params
 
 if [ "$SCHEMA_TYPE" == "mongo" ]; then
   curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-  mongo --ssl --host $DOCDB_ENDPOINT:27017 --sslCAFile global-bundle.pem --username $DOCDB_USERNAME --password $DOCDB_PASSWORD < /app/schema/$COMPONENT.js
+  mongo --ssl --host $DOCDB_ENDPOINT:27017 --sslCAFile global-bundle.pem --username $DOCDB_USERNAME --password $DOCDB_PASSWORD < /schema/$COMPONENT.js
 elif [ "$SCHEMA_TYPE" == "mysql" ]; then
-  mysql -h ${MYSQL_ENDPOINT} -u${MYSQL_USERNAME} -p${MYSQL_PASSWORD} </app/schema/$COMPONENT.sql
+  mysql -h ${MYSQL_ENDPOINT} -u${MYSQL_USERNAME} -p${MYSQL_PASSWORD} < /schema/$COMPONENT.sql
 else
   echo "Invalid Schema Input"
   exit 1
